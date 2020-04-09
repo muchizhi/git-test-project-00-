8f60f344d91a10157fb915fd8a8e54b7ea42c6d2
muchizhi <muchizhi@gmail.com>
2020-04-09 19:55:46 +0300

    Update readme.py
    внесенное изменение - комментарий

--- readme.py
+++ readme.py
@@ -2,3 +2,5 @@ Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit 
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> # тестовый реп
 >>> # описание кода
+
+>>> # внесенное изменение
